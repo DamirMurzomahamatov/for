a50e91f10f80bf7d4e1aa6901600801bfe062e77
DamirMurzomahamatov <you@example.com>
2022-03-21 15:37:52 +0500

    homework11

---
+++ index.js
@@ -0,0 +1,12 @@
+for (let  i = 1; i <= 9; i++) {
+	for (let  j = 1; j <= 3; j++) {
+		document.write(i); 
+	}
+}
+
+document.write('<br>')
+let str = '';
+for (let i = 0; i < 10; i++) {
+	str = str + '*';
+	document.write(str + '<br>');
+}
